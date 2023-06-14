b7b95b9b47e0446db39a44c7d6e8dee2a90ff9c4
theligero <iligero@ucm.es>
2023-06-14 18:00:34 +0200

    Dragones y mamporras
    cagoenlaputa

--- Junio22-Ej3/Junio22-Ej3.cpp
+++ Junio22-Ej3/Junio22-Ej3.cpp
@@ -1,63 +1,61 @@
 // Ignacio Ligero Martín
-// VJ25
-
+// 06023859K
+// Pto0204
 
 #include <iostream>
-#include <iomanip>
 #include <fstream>
-#include <vector>
+#include "bintree_eda.h"
+#include <climits>
 
-
-// O(log(n)), siendo n el número de eltos del vector
-int resolver(std::vector<int> const& v, int ini, int fin) {
-    int n = fin - ini;
-    if (n == 0) return v[ini];
-    else if (n == 1) return v[ini];
+// Tendrá complejidad de tipo O(n) puesto que, en el peor
+// de los casos, hará dos llamadas de tamaño mitad (que, en
+// resumidas cuentas, significará que recorrerá la totalidad
+// del árbol)
+int resuelve(bintree<int> arbol, int ac, int& minAc) {
+    int hijoIzq = INT_MAX;
+    int hijoDer = INT_MAX;
+    if (arbol.right().empty() && arbol.left().empty()) {
+        if (ac < minAc) { minAc = ac; return arbol.root(); }
+    }
     else {
-        int mitad = (fin + ini) / 2;
-        if (v[mitad] == v[ini] + mitad - ini) return resolver(v, mitad, fin);
-        else return resolver(v, ini, mitad);
+        if (!arbol.left().empty())
+            hijoIzq = resuelve(arbol.left(), ac + 1, minAc);
+        if (!arbol.right().empty())
+            hijoDer = resuelve(arbol.right(), ac + 1, minAc);
+
+        if (hijoIzq != INT_MAX && hijoIzq >= 3 && ac < minAc) return hijoIzq;
+        else if (hijoDer != INT_MAX && hijoDer >= 3 && ac < minAc) return hijoDer;
     }
 }
 
-// Resuelve un caso de prueba, leyendo de la entrada la
-// configuración, y escribiendo la respuesta
 void resuelveCaso() {
-    // leer los datos de la entrada
-    int tam;
-
-    std::cin >> tam;
-    std::vector<int> v(tam);
-
-    for (auto& i : v) {
-        std::cin >> i;
-    }
-
-    int sol = resolver(v, 0, v.size());
-
-    // escribir sol
-    std::cout << sol << std::endl;
+    bintree<int> arbol;
+    arbol = leerArbol(-1); // -1 es la repr. de arbol vacio
+    int aux = INT_MIN;
+    int ac = 0;
+    int minAc = INT_MAX;
+    aux = resuelve(arbol, ac, minAc);
+    if (aux != INT_MIN) std::cout << aux;
+    std::cout << std::endl;
 }
 
+//#define DOMJUDGE
 int main() {
     // Para la entrada por fichero.
     // Comentar para acepta el reto
 #ifndef DOMJUDGE
-    std::ifstream in("datos.txt");
+    std::ifstream in("input2.txt");
     auto cinbuf = std::cin.rdbuf(in.rdbuf()); //save old buf and redirect std::cin to casos.txt
-#endif 
-
-
+#endif
     int numCasos;
     std::cin >> numCasos;
     for (int i = 0; i < numCasos; ++i)
         resuelveCaso();
 
-
     // Para restablecer entrada. Comentar para acepta el reto
 #ifndef DOMJUDGE // para dejar todo como estaba al principio
     std::cin.rdbuf(cinbuf);
-    system("PAUSE");
+    //system("PAUSE");
 #endif
 
     return 0;
